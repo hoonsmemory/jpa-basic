1d5f5e55a944358b443949cbfab4d0d7e8a850b1
hoon <dltjdgnsok1@naver.com>
2022-03-28 23:03:52 +0900

    JPA 기본 문법 사용

--- src/main/java/jpa/basic/hello/JpaEx1.java
+++ src/main/java/jpa/basic/hello/JpaEx1.java
@@ -6,7 +6,7 @@ import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 import java.util.List;
 
-public class JpaMain {
+public class JpaEx1 {
 
     public static void main(String[] args) {
 
